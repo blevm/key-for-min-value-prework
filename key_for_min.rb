27275b6b34ec79b7a1a9416a4ab0d6baf5fb13ba
Michelle Blevins <mjblevins7@gmail.com>
2018-03-26 18:00:44 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.collect do |key, value|
-    if min_value == nil || value < min_value
+    if min_value == nil
+      min_value = value
+      min_key = key
+    elsif value < min_value
       min_value = value
       min_key = key
     end
